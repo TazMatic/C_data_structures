468027772082b5626b548eb9a4ebc39bbd6ee0f2
Taylor Morse <taylorabc101@gmail.com>
2020-12-13 19:17:07 -0800

    Add mutex locks to functions

--- single_linked_list_threaded/src/single_linked_list_threaded.c
+++ single_linked_list_threaded/src/single_linked_list_threaded.c
@@ -1,7 +1,10 @@
+#include <pthread.h> 
+
 #include "single_linked_list_threaded.h"
 
 struct single_linked_list_threaded {
     struct node *head;
+    pthread_mutex_t mutexLock;
 };
 
 struct node
@@ -33,12 +36,19 @@ single_linked_list_threaded_size(const single_linked_list_threaded *list)
         return 0;
     }
     size_t len = 0;
+    
+    // Have to lock here since the head could move during the setup which would cause a bad count
+    pthread_mutex_lock(&list->mutexLock); 
+    
     struct node *currentNode = list->head;
     while(currentNode)
     {
         ++len;
         currentNode = currentNode->next;
     }
+    
+    pthread_mutex_unlock(&list->mutexLock);
+    
     return len;
 }
 
@@ -57,7 +67,10 @@ single_linked_list_threaded_append(single_linked_list_threaded *list, double val
     }
     newNode->value = value;
     newNode->next = NULL;
-    // Iterate over the list to the last node
+    
+    // Iterate over the list to the last node and obtain the lock here so the head cant change after we start
+    pthread_mutex_lock(&list->mutexLock); 
+    
     struct node *currentNode = list->head;
     if(!currentNode)
     {
@@ -69,6 +82,9 @@ single_linked_list_threaded_append(single_linked_list_threaded *list, double val
         currentNode = currentNode->next;
     }
     currentNode->next = newNode;
+    
+    pthread_mutex_unlock(&list->mutexLock);
+
     return true;
 }
 
@@ -86,10 +102,16 @@ single_linked_list_threaded_prepend(single_linked_list_threaded *list, double va
         return false;
     }
     newNode->value = value;
+    
+    pthread_mutex_lock(&list->mutexLock); 
+
     newNode->next = list->head;
 
     // Point old list head to new node
     list->head = newNode;
+    
+    pthread_mutex_unlock(&list->mutexLock);
+
     return true;
 }
 
@@ -100,6 +122,10 @@ single_linked_list_threaded_insertAfterIndex(single_linked_list_threaded *list, 
     {
         return false;
     }
+    
+    // Locking here assures that the order and position of the list nodes dont change when iterating through it.
+    pthread_mutex_lock(&list->mutexLock); 
+
     struct node *currentNode = list->head;
     while (currentNode && idx > 0)
     {
@@ -119,6 +145,9 @@ single_linked_list_threaded_insertAfterIndex(single_linked_list_threaded *list, 
     newNode->next = currentNode->next;
 
     currentNode->next = newNode;
+    
+    pthread_mutex_unlock(&list->mutexLock);
+
     return true;
 }
 
@@ -130,15 +159,23 @@ single_linked_list_threaded_fetch(single_linked_list_threaded *list, size_t idx)
         return NULL;
     }
     struct node *currentNode = list->head;
+
+    pthread_mutex_lock(&list->mutexLock); 
+
     while (currentNode && idx > 0)
     {
         --idx;
         currentNode = currentNode->next;
     }
+
+    // Release the lock here since we dont need it below and it has to be before the return
+    pthread_mutex_unlock(&list->mutexLock);
+
     if (idx != 0)
     {
         return NULL;
     }
+
     return &currentNode->value;
 }
 
@@ -149,12 +186,17 @@ single_linked_list_threaded_destroy(single_linked_list_threaded *list){
         return;
     }
     struct node *currentNode = list->head;
+
+    pthread_mutex_lock(&list->mutexLock); 
+
     while(currentNode)
     {
         struct node *nextNode = currentNode->next;
         free(currentNode);
         currentNode = nextNode;
     }
+    pthread_mutex_unlock(&list->mutexLock);
+    pthread_mutex_destroy(list->head);
     free(list);
 
 }
